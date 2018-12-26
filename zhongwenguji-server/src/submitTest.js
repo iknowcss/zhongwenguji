883494113408fd87b0c55943d67bacc2bec7db9e
Cody Jenkins <cjenkins@dius.com.au>
2018-12-26 15:13:15 +1100

    First stab at curve fit

--- zhongwenguji-server/src/submitTest.js
+++ zhongwenguji-server/src/submitTest.js
@@ -1,8 +1,42 @@
 const { Router } = require('express');
 const bodyParser = require('body-parser');
+const fminsearch = require('./analyze/fminsearch');
 
-function getCurveParameters() {
-  return {};
+function model([amplitude, decayStartX, decayPeriod], xi) {
+  if (xi < decayStartX) {
+    return amplitude;
+  } else if (xi >= decayStartX + decayPeriod) {
+    return 0;
+  }
+  return amplitude / 2 * (1 + Math.cos((xi - decayStartX) * Math.PI / decayPeriod));
+}
+
+const fun = (x, P) => x.map(model.bind(null, P));
+
+function rangeMidpoint([a, b]) {
+  return (a + b) / 2;
+}
+
+function getCurveParameters(testData) {
+  const x = [];
+  const y = [];
+
+  testData.forEach((section) => {
+    if (!section.isTested) {
+      return;
+    }
+    try {
+      const xi = rangeMidpoint(section.range);
+      const yi = section.knownPercent || 0;
+      x.push(xi);
+      y.push(yi);
+    } catch (error) {
+      console.warn('Failed to calculate coordinates from section', { section, error });
+    }
+  });
+
+  const [amplitude, decayStartX, decayPeriod] = fminsearch(fun, [100, 500, 200], x, y);
+  return { amplitude, decayStartX, decayPeriod };
 }
 
 module.exports = () => {
--- zhongwenguji-server/src/submitTest.test.js
+++ zhongwenguji-server/src/submitTest.test.js
@@ -11,7 +11,7 @@ describe('submitTest', () => {
       .send({
         testData: [
           { isTested: true, knownPercent: 100, range: [0, 250] },
-          { isTested: true, knownPercent: 90, range: [250, 500] },
+          { isTested: false, knownPercent: NaN, range: [250, 500] },
           { isTested: true, knownPercent: 50, range: [500, 750] },
           { isTested: true, knownPercent: 10, range: [750, 1000] },
           { isTested: true, knownPercent: 0, range: [1000, 1250] },
@@ -20,8 +20,12 @@ describe('submitTest', () => {
           { isTested: false, knownPercent: NaN, range: [1750, 2000] }
         ]
       })
-      .expect(200, {
-        curveParams: {}
+      .expect(200)
+      .then(({ body }) => {
+        expect(typeof body.curveParams.amplitude).toBe('number');
+        expect(typeof body.curveParams.decayStartX).toBe('number');
+        expect(typeof body.curveParams.decayPeriod).toBe('number');
+        expect(body.curveParams).toMatchSnapshot();
       });
   });
 });
