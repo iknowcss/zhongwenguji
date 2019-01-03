95b334a98a2e44f858e36f42c67769b84b47ee3e
Cody Jenkins <iknowcss@gmail.com>
2019-01-03 12:24:34 +1100

    Add attributions

--- README.md
+++ README.md
@@ -14,4 +14,14 @@
 
 # Credits
 
-Icons made by <a href="https://www.flaticon.com/authors/egor-rumyantsev" title="Egor Rumyantsev">Egor Rumyantsev</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+Non-linear regression algorithm used with permission from Dr. Jonasal Meida's 
+[`fminsearch`](https://github.com/jonasalmeida/fminsearch).
+
+Icons made by:
+
+* <a href="https://www.flaticon.com/authors/egor-rumyantsev" title="Egor Rumyantsev">Egor Rumyantsev</a>
+    from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    (licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>) 
+* <a href="https://www.freepik.com/" title="Freepik">Freepik</a>
+    from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    (licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>)
--- zhongwenguji-server/src/analyze/fminsearch.js
+++ zhongwenguji-server/src/analyze/fminsearch.js
@@ -1,23 +1,32 @@
 /* Modified from:
  * https://github.com/jonasalmeida/fminsearch
+ */
+
+/*
+ * MIT License
+ *
+ * Copyright (c) Jonas Almeida.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *
  */
 
 function fminsearch(fun, Parm0, x, y, Opt) {
-  // fun = function(x,Parm)
-  // example
-  //
-  // x = [32,37,42,47,52,57,62,67,72,77,82,87,92];y=[749,1525,1947,2201,2380,2537,2671,2758,2803,2943,3007,2979,2992]
-  // fun = function(x,P){return x.map(function(xi){return (P[0]+1/(1/(P[1]*(xi-P[2]))+1/P[3]))})}
-  // Parms=jmat.fminsearch(fun,[100,30,10,5000],x,y)
-  //
-  // Another test:
-  // x=[32,37,42,47,52,57,62,67,72,77,82,87,92];y=[0,34,59,77,99,114,121,133,146,159,165,173,170];
-  //
-  // Opt is an object will all other parameters, from the objective function (cost function), to the 
-  // number of iterations, initial step vector and the display switch, for example
-  // Parms=fminsearch(fun,[100,30,10,5000],x,y,{maxIter:10000,display:false})
-  
   if(!Opt){Opt={}};
   if(!Opt.maxIter){Opt.maxIter=1000};
   if(!Opt.step){// initial step is 1/100 of initial value (remember not to use zero in Parm0)
