dd37370114605fed31f89687201f325aa1f1f2a7
Cody Jenkins <iknowcss@gmail.com>
2019-01-14 22:36:55 +1100

    List missing character definitions

--- data/select.js
+++ data/select.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const { promisify } = require('util');
 const sqlite3 = require('sqlite3');
 
@@ -19,11 +20,34 @@ db.serialize(async () => {
   const selectByTrad = promisify(selectByTradStatement.all.bind(selectByTradStatement));
   const selectBySimp = promisify(selectBySimpStatement.all.bind(selectBySimpStatement));
 
-  console.time('dbread');
-  console.log(await selectByTrad('我'));
-  console.timeEnd('dbread');
+  const allCharacters = fs
+    .readFileSync(path.join(__dirname, '../server/all-characters.txt'))
+    .toString()
+    .split('\n')
+    .map(x => {
+      const [i, c, , , p, d] = x.split('\t');
+      return { i, c, p, d };
+    });
 
-  console.time('dbread');
-  console.log(await selectBySimp('在'));
-  console.timeEnd('dbread');
+  let missingCharacter = 0;
+  for (let i = 0; i < allCharacters.length; i++) {
+    const x = allCharacters[i];
+    if (!x.c) continue;
+    let cedictEntry = await selectByTrad(x.c);
+    if (cedictEntry.length === 0) {
+      cedictEntry = await selectBySimp(x.c);
+    }
+    if (cedictEntry.length === 0) {
+      missingCharacter++;
+      process.stdout.write(x.c);
+      if (missingCharacter % 40 === 0) process.stdout.write('\n');
+      // console.log('Could not find character', x.c);
+    } else {
+      x.c = cedictEntry[0].simp;
+    }
+  }
+  process.stdout.write('\n');
+
+  console.log('missingCharacter', missingCharacter)
+  console.log('allCharacters.length', allCharacters.length)
 });
