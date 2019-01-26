6c6e5eada2f6d118a2aaf0bf168d8debfd54127c
Cody Jenkins <iknowcss@gmail.com>
2019-01-27 08:43:27 +1100

    Initial missed cards UI

--- client/src/results/MissedCards.js
+++ client/src/results/MissedCards.js
@@ -4,16 +4,47 @@ import connect from 'react-redux/es/connect/connect';
 import mapSelectors from '../util/mapSelectors';
 import { missedCards } from '../characterTest/characterTestReducer';
 import {loadSamples} from '../characterTest/characterTestActions';
+import formatPinyin from '../util/formatPinyin';
 
-const MissedCards = ({ missedCards = [] } = {}) => (
-  <div className={style.container}>
-    <ul>
-      {missedCards.map(({ index, character, definition }) => (
-        <li className={style.listItem} key={index}>{character} - {definition.join('***')}</li>
+function pairPinyinDefinitions(pinyinArray, definitionArray) {
+  const result = [];
+  for (let i = 0; i < pinyinArray.length; i++) {
+    const pinyin = pinyinArray[i];
+    const definition = [definitionArray[i]];
+    result.push({ pinyin, definition })
+  }
+  return result;
+}
+
+function formatDefinition(definition) {
+  return definition.replace(/\//g, ' / ');
+}
+
+const MissedCards = ({ missedCards = [] } = {}) => {
+  return (
+    <div className={style.container}>
+      {missedCards.map((card) => (
+        <div key={card.index} className={style.cardRow}>
+          <div className={style.cardCharacterColumn}>
+            <span>{card.character}</span>
+          </div>
+          <div className={style.cardPinyinDefColumn}>
+            {pairPinyinDefinitions(card.pinyin, card.definition).map(({ pinyin, definition }, i) => (
+              <div key={i} className={style.cardPinyinDefRow}>
+                <div className={style.cardPinyinColumn}>
+                  <span>{formatPinyin(pinyin)}</span>
+                </div>
+                <div className={style.cardDefColumn}>
+                  <span>{formatDefinition(definition[0])}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       ))}
-    </ul>
-  </div>
-);
+    </div>
+  );
+};
 
 export { MissedCards as Pure };
 
