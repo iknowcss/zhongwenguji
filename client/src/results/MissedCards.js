b9efa018e8c53a099f96bce8c05721c76999013d
Cody Jenkins <iknowcss@gmail.com>
2019-01-27 17:13:50 +1100

    Display missed cards buttons

--- client/src/results/MissedCards.js
+++ client/src/results/MissedCards.js
@@ -5,6 +5,7 @@ import mapSelectors from '../util/mapSelectors';
 import { missedCards } from '../characterTest/characterTestReducer';
 import {loadSamples} from '../characterTest/characterTestActions';
 import formatPinyin from '../util/formatPinyin';
+import Button from '../component/Button';
 
 function pairPinyinDefinitions(pinyinArray, definitionArray) {
   const result = [];
@@ -23,25 +24,33 @@ function formatDefinition(definition) {
 const MissedCards = ({ missedCards = [] } = {}) => {
   return (
     <div className={style.container}>
+      <div className={style.cardRowContainer}>
       {missedCards.map((card) => (
-        <div key={card.index} className={style.cardRow}>
-          <div className={style.cardCharacterColumn}>
-            <span>{card.character}</span>
-          </div>
-          <div className={style.cardPinyinDefColumn}>
-            {pairPinyinDefinitions(card.pinyin, card.definition).map(({ pinyin, definition }, i) => (
-              <div key={i} className={style.cardPinyinDefRow}>
-                <div className={style.cardPinyinColumn}>
-                  <span>{formatPinyin(pinyin)}</span>
+        <div key={card.index} className={style.cardRowColor}>
+          <div className={style.cardRow}>
+            <div className={style.cardCharacterColumn}>
+              <span>{card.character}</span>
+            </div>
+            <div className={style.cardPinyinDefColumn}>
+              {pairPinyinDefinitions(card.pinyin, card.definition).map(({ pinyin, definition }, i) => (
+                <div key={i} className={style.cardPinyinDefRow}>
+                  <div className={style.cardPinyinColumn}>
+                    <span>{formatPinyin(pinyin)}</span>
+                  </div>
+                  <div className={style.cardDefColumn}>
+                    <span>{formatDefinition(definition[0])}</span>
+                  </div>
                 </div>
-                <div className={style.cardDefColumn}>
-                  <span>{formatDefinition(definition[0])}</span>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       ))}
+      </div>
+      <div className={style.actionButtonContainer}>
+        <Button secondary className={style.actionButton}>Back</Button>
+        <Button className={style.actionButton}>Add to Skritter</Button>
+      </div>
     </div>
   );
 };
