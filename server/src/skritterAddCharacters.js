365bcf6c2dce0c46ebe43dcb475ad37e21f5673c
Cody Jenkins <cjenkins@dius.com.au>
2019-02-06 10:00:12 +1100

    Add empty section

--- server/src/skritterAddCharacters.js
+++ server/src/skritterAddCharacters.js
@@ -2,10 +2,11 @@ const { URL } = require('url');
 const request = require('request');
 const httpStatus = require('http-status');
 const bodyParser = require('body-parser');
+const cloneDeep = require('lodash/cloneDeep');
 const { decryptObject } = require('./cryptoService');
 
 const SKRITTER_API_BASE_URL = 'https://legacy.skritter.com/api/v0/';
-const VOCAB_LIST_NAME = 'Hanzi Shan - Missed Characters';
+const VOCAB_LIST_NAME = 'HanziShan Missed Characters';
 
 function buildAuthHeaders(session) {
   return {
@@ -42,12 +43,12 @@ function apiCall(what, { session, url, headers, validStatusCodes = [200, 201], .
   });
 }
 
-async function getHanziShanVocabList(session) {
+async function findHanziShanVocabList(session) {
   const url = new URL('vocablists', SKRITTER_API_BASE_URL);
   url.searchParams.set('sort', 'custom');
   url.searchParams.set('fields', 'id,name,creator');
 
-  const { VocabLists: vocabLists } = await apiCall('Skritter vocab lists', { url, session });
+  const { VocabLists: vocabLists } = await apiCall('search Skritter vocab lists', { url, session });
   const matchingLists = vocabLists
     .filter(({ creator, name }) => creator === session.userId && name === VOCAB_LIST_NAME);
 
@@ -63,13 +64,23 @@ async function getHanziShanVocabList(session) {
   }
 }
 
+async function getHanziShanVocabList(session, vocabListId) {
+  const url = new URL(`vocablists/${vocabListId}`, SKRITTER_API_BASE_URL);
+  url.searchParams.set('fields', 'id,name,creator,sections');
+
+  const { VocabList: vocabList } = await apiCall('get Skritter vocab list', { url, session });
+
+  return vocabList;
+}
+
 async function createHanziShanVocablist(session) {
   const body = {
     name: VOCAB_LIST_NAME,
     lang: 'zh',
     description: 'Hanzi Shan missed character review list',
     creator: session.userId,
-    tags: ['hanzishan']
+    tags: ['hanzishan'],
+    sections: []
   };
 
   const url = new URL('vocablists', SKRITTER_API_BASE_URL);
@@ -83,20 +94,41 @@ async function createHanziShanVocablist(session) {
   return vocabList;
 }
 
-async function ensureHanziShanVocabList(session) {
-  let vocabList = await getHanziShanVocabList(session);
-  if (!vocabList) {
-    console.info('Create vocab list for user', { userId: session.userId });
-    vocabList = createHanziShanVocablist(session);
-  } else {
-    console.info('Vocab list already exists for user', { userId: session.userId });
-  }
-  return vocabList;
+
+function createSection(index = 0, characters) {
+  return {
+    name: `Test ${index + 1}`,
+    rows: []
+  };
+}
+
+async function addHanzishanVocabListSection(session, existingVocabList, characters) {
+  const url = new URL(`vocablists/${existingVocabList.id}`, SKRITTER_API_BASE_URL);
+  const body = {
+    studyingMode: 'adding',
+    sections: cloneDeep(existingVocabList.sections)
+  };
+  body.sections.push(createSection(body.sections.length, characters));
+
+  await apiCall('add section to Skritter vocab list', {
+    session,
+    url,
+    method: 'PUT',
+    body
+  });
 }
 
 async function addCharactersToVocabList(session, characters) {
-  const { id: vocabListId } = await ensureHanziShanVocabList(session);
-  
+  let vocabList = await findHanziShanVocabList(session);
+  if (!vocabList) {
+    console.info('Create vocab list for user', { userId: session.userId });
+    vocabList = await createHanziShanVocablist(session);
+  } else {
+    console.info('Vocab list already exists for user', { userId: session.userId, vocabListId: vocabList.id });
+    vocabList = await getHanziShanVocabList(session, vocabList.id);
+  }
+
+  await addHanzishanVocabListSection(session, vocabList, characters);
 }
 
 module.exports = () => [
