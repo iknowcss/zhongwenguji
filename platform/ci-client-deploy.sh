6c76a80f135c22cc485eff21533a826dd19bf6f6
Cody Jenkins <cjenkins@dius.com.au>
2019-10-16 18:02:36 +1100

    Revert "Use cloudfront for content distributrion (#17)"
    This reverts commit 64bbe36972a479b926915960fbfa257885b0b595.

--- platform/ci-client-deploy.sh
+++ platform/ci-client-deploy.sh
@@ -3,24 +3,16 @@
 set -e
 
 projectRoot=$1
-if [[ -z "$projectRoot" ]]
-then
+if [[ -z "$projectRoot" ]]; then
     projectRoot='.'
 fi
 
-if [ "$STAGE" = "staging" ]
-then
-  PUBLIC_URL="/"
-  DEPLOY_S3_BUCKET=staging.hanzishan.com
-elif [ "$STAGE" = "production" ]
-then
-  PUBLIC_URL="https://d3s6t86za6yn71.cloudfront.net/"
-  DEPLOY_S3_BUCKET=hanzishan.com
-fi
-
 # Make env variables available to react build
-export REACT_APP_STAGE="$STAGE"
-export PUBLIC_URL="$PUBLIC_URL"
+export PUBLIC_URL="/"
+export REACT_APP_STAGE=$STAGE
+
+[ "$STAGE" = "staging" ] && DEPLOY_S3_BUCKET=staging.hanzishan.com
+[ "$STAGE" = "production" ] && DEPLOY_S3_BUCKET=hanzishan.com
 
 echo "Deploy client to S3..."
 (
