2f4788db33b54dca6b2438bdfb064c88def5cf4a
Cody Jenkins <cjenkins@dius.com.au>
2020-06-21 19:20:45 +1000

    Finish documenting submitTest

--- server/src/analyze/submitTest.js
+++ server/src/analyze/submitTest.js
@@ -1,9 +1,11 @@
 const fitModelToData = require('./fitModelToData');
 
+module.exports.fitModelToMarkings = fitModelToMarkings;
+
 /**
  * @typedef GraphData
  * @property {{x: number, y: number}[]} samplePoints
- * @property {{x: number, y: number}[]} modelFitPoints - Terminated after 2 instances of y = 0.
+ * @property {{x: number, y: number}[]} modelFitPoints
  *
  */
 
@@ -15,8 +17,9 @@ const fitModelToData = require('./fitModelToData');
  */
 
 /**
- * Takes the character markings for the provided set of bins, logs them to the database and
- * returns an estimate, error, and graph data
+ * Takes the character markings for the provided set of bins, logs them to the database and returns an estimate, error,
+ * and graph data. The error precision is limited to one and rounded where necessary. The estimation precision is
+ * rounded to the order of magnitude of the error.
  *
  * @param {FrequencyEntry[]} entries - The set of {CharacterEntry}s to sample from sorted ascending by Character
  *    identifier.
@@ -36,7 +39,7 @@ function fitModelToMarkings(entries, binSampleParameters, markedEntries) {
     binResults[binIndex].push(entry.known ? 1 : 0);
   });
 
-  const binIndexMidpoint = binIndexMidpointCalculator(binSize);
+  const binIndexMidpoint = buildBinEntryMidpointCalculator(binSize);
   // Each x-value is the midpoint of the entry frequencies for the first hypothetical entry in the bin and the last
   // hypothetical entry in the bin. For example, when the binSize is 4, the 0th bin would have hypothetical entries
   // [1, 2, 3, 4]. The of this bin would be 2.5.
@@ -48,13 +51,13 @@ function fitModelToMarkings(entries, binSampleParameters, markedEntries) {
     return binEntryScore.reduce(sum) / binEntryScore.length;
   });
 
-  // Fit the model to the sample data.
-  const curveParameters = fitModelToData(model, [1, 1, 1000], sampleXValues, sampleYValues);
-  const modelFunction = buildModelFunction(curveParameters);
+  // Fit a cosine decay function to the sample data.
+  const curveParameters = fitModelToData(parameterizedCosineDecay, [1, 1, 1000], sampleXValues, sampleYValues);
+  const modelFunction = buildCosineDecay(curveParameters);
   const modelXValues = Array.from({ length: binCount + 1 }, (n, binIndex) => binIndex * binSize);
   const modelYValues = modelXValues.map(modelFunction);
 
-  // Zip x and y arrays for sample and model curve points to coordinate pairs for graphing.
+  // Zip x and y arrays for sample and cosine decay curve points to coordinate pairs for graphing.
   const graphData = {
     samplePoints: sampleXValues.map((x, i) => ({ x, y: 100 * sampleYValues[i] })),
     modelFitPoints: modelXValues.map((x, i) => ({ x, y: 100 * modelYValues[i] })),
@@ -68,11 +71,11 @@ function fitModelToMarkings(entries, binSampleParameters, markedEntries) {
       (_, infIndex) => modelFunction(dx * infIndex) * dx,
     )
     .reduce(sum);
-  const curveAreaError = calculateUncertainty(modelFunction, binSize, totalCharacterCount, sampleXValues, sampleYValues);
+  const curveAreaError = calculateUncertainty(modelFunction, totalCharacterCount, sampleXValues, sampleYValues);
 
   // Significant digits
-  const minUncertanty = toPrecision(1, curveArea * 0.05);
-  const knownEstimateError = Math.max(minUncertanty, Number.parseFloat((curveAreaError).toPrecision(1)));
+  const minUncertainty = toPrecision(1, curveArea * 0.05);
+  const knownEstimateError = Math.max(minUncertainty, Number.parseFloat((curveAreaError).toPrecision(1)));
   const significantDigits = Math.floor(log10(curveArea)) - Math.floor(log10(knownEstimateError)) + 1;
   const knownEstimate = toPrecision(significantDigits, curveArea);
 
@@ -80,21 +83,23 @@ function fitModelToMarkings(entries, binSampleParameters, markedEntries) {
 }
 
 /**
- * @param {function(number): number} modelFunction
- * @param {number} binSize
- * @param {number} totalCharacterCount
- * @param {number[]} sampleXValues
- * @param {number[]} sampleYValues
+ * Calculates the total uncertainty of the the estimate of all known characters derived from the model function.
+ *
+ * @private
+ * @param {function(number): number} modelFunction - The fitted model function.given by the
+ * @param {number} totalEntryCount - Number of entries available to sample for running the test.
+ * @param {number[]} sampleXValues - The independent variable values used for the model fit.
+ * @param {number[]} sampleYValues - The dependent variable values used for the model fit.
  */
-function calculateUncertainty(modelFunction, binSize, totalCharacterCount, sampleXValues, sampleYValues) {
+function calculateUncertainty(modelFunction, totalEntryCount, sampleXValues, sampleYValues) {
   const normalizedErrors = sampleXValues.map((sampleXValue, i) => Math.abs(sampleYValues[i] - modelFunction(sampleXValue)));
   const normalizedErrorMean = normalizedErrors.reduce(sum) / normalizedErrors.length;
 
   // Calculate error area
-  const dx = binSize / 100;
+  const dx = 10;
   return Array
     .from(
-      { length: Math.floor(totalCharacterCount / dx) },
+      { length: Math.floor(totalEntryCount / dx) },
       (_, infIndex) => {
         const modelXValue = dx * infIndex;
         const minBound = modelFunction(modelXValue) - normalizedErrorMean;
@@ -104,7 +109,16 @@ function calculateUncertainty(modelFunction, binSize, totalCharacterCount, sampl
     .reduce(sum);
 }
 
-const binIndexMidpointCalculator = binSize => (binIndex) => {
+/**
+ * Returns a function which assumes the specified bin size. That function calculates the hypothetical entry id of the
+ * entry at the middle of the bin with the given index. If the bin size is odd, the hypothetical entry id will end in
+ * {.5}.
+ *
+ * @private
+ * @param binSize - The bin size to
+ * @returns {function(number|string): number}
+ */
+const buildBinEntryMidpointCalculator = binSize => (binIndex) => {
   const bi = parseInt(binIndex, 10);
   const firstEntryIndex = binSize * bi + 1;
   const lastEntryIndex = binSize * bi + binSize;
@@ -112,59 +126,64 @@ const binIndexMidpointCalculator = binSize => (binIndex) => {
 };
 
 /**
+ * Cosine decay function with adjustable parameters for {amplitude}, {decayStart}, and {decayPeriod}. For inputs from
+ * {-Infinity} to {decayStart}, the output is equal to {amplitude} (constant). From {decayStart} to
+ * {decayStart + decayPeriod}, the output decays from {amplitude} to {0} following a cosine from {0} to {π}. For inputs
+ * greater than {decayStart + decayPeriod}, the output is {0}. 
+ *
+ * @private
+ * @param {number[]} modelParams - A tuple of parameters for the function.
+ * @param {number} modelParams[0] - {amplitude}: the maximum output of the function.
+ * @param {number} modelParams[1] - {decayStart}: The input value after which the decay starts.
+ * @param {number} modelParams[2] - {decayPeriod}: The width of the domain after {decayStart} over which the output 
+ *    decays to {0}.
+ * @param {number} input - The function input.
+ * @returns {number}
+ */
+function parameterizedCosineDecay(modelParams, input) {
+  const [amplitude, decayStartX, decayPeriod] = modelParams;
+  if (input < decayStartX) {
+    return amplitude;
+  } else if (input >= decayStartX + decayPeriod) {
+    return 0;
+  }
+  return amplitude / 2 * (1 + Math.cos((input - decayStartX) * Math.PI / decayPeriod));
+}
+
+/**
+ * Creates a cosine decay function with fixed parameters. (see {@link parameterizedCosineDecay}).
+ * 
+ * @private
+ * @param {number[]} modelParams
+ * @returns {function(number): number}
+ */
+const buildCosineDecay = (modelParams) => parameterizedCosineDecay.bind(null, modelParams);
+
+/**
+ * Sum two numbers together. If a number is {undefined}, treat it as {0}. Useful while `reduce()`ing lists.
+ *
+ * @private
  * @param {number|undefined} a
  * @param {number|undefined} b
  * @returns {number}
  */
 const sum = (a, b) => (a || 0) + (b || 0);
 
+/**
+ * Convenience function for base-10 log.
+ *
+ * @private
+ * @param {number} x
+ * @returns {number}
+ */
 const log10 = x => Math.log(x) / Math.log(10);
 
+/**
+ * Reduces the precision of a {number} to the specified number of digits.
+ *
+ * @private
+ * @param {number} precision - The number to adjust the precision of.
+ * @param {number} n - The number of digits to limit the precision to.
+ * @returns {number}
+ */
 const toPrecision = (precision, n) => Number.parseFloat(n.toPrecision(precision));
-
-module.exports.fitModelToMarkings = fitModelToMarkings;
-
-/// - Old Stuff --------------------------------------------------------------------------------------------------------
-
-function model([amplitude, decayStartX, decayPeriod], xi) {
-  if (xi < decayStartX) {
-    return amplitude;
-  } else if (xi >= decayStartX + decayPeriod) {
-    return 0;
-  }
-  return amplitude / 2 * (1 + Math.cos((xi - decayStartX) * Math.PI / decayPeriod));
-}
-
-const buildModelFunction = ([amplitude, decayStartX, decayPeriod]) => (xi) => {
-  if (xi < decayStartX) {
-    return amplitude;
-  } else if (xi >= decayStartX + decayPeriod) {
-    return 0;
-  }
-  return amplitude / 2 * (1 + Math.cos((xi - decayStartX) * Math.PI / decayPeriod));
-};
-
-
-function rangeMidpoint([a, b]) {
-  return (a + b) / 2;
-}
-
-function getCurveParameters(testData) {
-  const xValues = [];
-  const yValues = [];
-  testData.filter(({ isTested }) => isTested).forEach((section) => {
-    try {
-      xValues.push(rangeMidpoint(section.range));
-      yValues.push(section.knownPercent || 0);
-    } catch (error) {
-      console.warn('Failed to calculate coordinates from section', { section, error });
-    }
-  });
-
-  const [amplitude, decayStartX, decayPeriod] = fitModelToData(model, [100, 1, 1000], xValues, yValues);
-  return { amplitude, decayStartX, decayPeriod };
-}
-
-module.exports.rangeMidpoint = rangeMidpoint;
-module.exports.getCurveParameters = getCurveParameters;
-module.exports.buildCurve = buildModelFunction;
